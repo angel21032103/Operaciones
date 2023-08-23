5a5d49853f19f5b36bc444bfa76aa9272def35ec
Alumno <Alumno@L23-2543295>
2023-08-23 11:52:59 -0600

    Agregar archivos de proyecto.

---
+++ Restar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    internal class Restar : Operacion
+    {
+        public int operar(int v1,int v2)
+        {
+            valor1 = v1;    //se pude acceder a los atributos directamente
+            valor2 = v2;    //por la relacion de herencia
+            return resultado = valor1 - valor2;
+        }
+    }
+}
---
+++ Sumar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    internal class Sumar : Operacion    //herencia de la clase operacion
+    {
+        public int operar(int v1,int v2)
+        {
+            valor1 = v1;    //se puede acceder a los atributos directamente
+            valor2 = v2;    //porla relacion de herencia
+            return resultado = valor1 + valor2; 
+        }
+    }
+}
